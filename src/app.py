dfeead63ff43fb90269355d4b32fa8695a907cb5
Styoks <113370103+Styoks@users.noreply.github.com>
2024-07-04 16:59:54 +0000

    TryExcept

--- src/app.py
+++ src/app.py
@@ -2,13 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +35,116 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    users = User.query.all()
+
+    if (users == ""):
+        return "This user doesn't exist or is empty"
+    
+    users_serialized = list(map(lambda item:item.serialize(), users))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Ok",
+        "data": users_serialized
     }
 
     return jsonify(response_body), 200
 
+@app.route('/user', methods=['POST'])
+def create_user():
+
+    new_user = request.json
+    if (new_user["name"]=="" or new_user["surname"] == "" or new_user["email"] == "") :
+        return "All field are required", 400
+    
+    try:
+        me =  User(name=new_user["name"], surname=new_user["surname"], email=new_user["email"], sub_date=new_user["sub_date"])
+        db.session.add(me)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Ok"
+        }
+        
+        return jsonify(response_body), 200
+    
+    except:
+        print("An exception occurred")
+        return "AAAAAAAAAAAAA", 500
+
+
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+
+    characters = Characters.query.all()
+    characters_serialized = list(map(lambda item:item.serialize(), characters))
+
+    response_body = {
+        "msg": "Ok",
+        "data": characters_serialized
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_single_character(character_id):
+
+    character = Characters.query.filter_by(id = character_id).first()
+    character_serialized = character.serialize()
+
+    response_body = {
+        "msg": "Ok",
+        "data": character_serialized
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/characters', methods=['POST'])
+def add_character():
+
+    new_character = request.json
+    character =  Characters(name=new_character["name"], hair=new_character["hair"], race=new_character["race"], years_old=new_character["years_old"], gender=new_character["gender"])
+    db.session.add(character)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Ok"
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/characters/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+    
+    character = Characters.query.filter_by(id = character_id).first()
+    db.session.delete(character)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Ok"
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+
+    planets = Planets.query.all()
+    planets_serialized = list(map(lambda item:item.serialize(), planets))
+
+    response_body = {
+        "msg": "Ok",
+        "data": planets_serialized
+    }
+
+    return jsonify(response_body), 200
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -3,17 +3,91 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    surname = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    sub_date = db.Column(db.String(250), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "surname": self.surname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "sub_date" : self.sub_date
         }
+
+class Planets(db.Model):
+    __tablename__ = 'Planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(250),nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planets_name": self.planet_name
+        }
+    
+class Characters(db.Model):
+    __tablename__ = 'Characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False)
+    hair = db.Column(db.String(250),nullable=False)
+    race = db.Column(db.String(250),nullable=False)
+    years_old = db.Column(db.Integer,nullable=False)
+    gender = db.Column(db.String(250),nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair": self.hair,
+            "race": self.race,
+            "years_old": self.years_old,
+            "gender": self.gender,
+        }
+
+# class Fav_Characters():
+#     __tablename__ = 'Fav_Characters'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('User.id'),nullable=False)
+#     character_id = Column(Integer, ForeignKey('Characters.id'),nullable=False)
+
+# class Vehicles():
+#     __tablename__ = 'Vehicles'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     model = Column(String(250),nullable=True)
+#     vehicle_name = Column(String(250),nullable=False)
+
+# class Fav_Vehicles():
+#     __tablename__ = 'Fav_Vehicles'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('User.id'),nullable=False)
+#     vehicle_id = Column(Integer, ForeignKey('Vehicles.id'),nullable=False)
+
+
+# class Fav_Planets():
+#     __tablename__ = 'Fav_Planets'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('User.id'),nullable=False)
+#     planet_id = Column(Integer, ForeignKey('Planets.id'),nullable=False)
+
+
+#    def to_dict(self):
+ #       return {}
